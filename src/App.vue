3e28eb4bf94568527d5bea1639c73372d88d8dce
Beatriz Bastos <biaborges.estudante@gmail.com>
2024-07-04 14:57:23 -0300

    Initial

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import Banner from './components/Banner.vue';
+
+export default {
+  components: { Banner : Banner }
+}
+</script>
+
+<template>
+  <Banner />
+
+  <h1 class="titulo">
+    Texto do App.vue
+  </h1>
+</template>
+
+<style scoped>
+
+</style>
